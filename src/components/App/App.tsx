96f7d7bb7094f754392442a6fdaee0295e036a57
Mark Cawthray <dr.cawthray@gmail.com>
2021-09-11 16:16:25 -0600

    Move app tsc test and css files into components folder

---
+++ src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../src/assets/logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>What In The World</h1>
+    </div>
+  );
+}
+
+export default App;
