ebec49cab74b5f12b4aa7e3650dcf4a72eb1e22c
Mark Cawthray <dr.cawthray@gmail.com>
2021-09-12 13:32:04 -0600

    Add notes in components

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -11,3 +11,7 @@ const Form = () => {
 }
 
 export default Form;
+//Holds a label of the question, an input field set to number | string, and a submit button
+// will need a handleChange function and handleClick function
+// This will then route to Answers Component on click
+// testing needed
--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -5,9 +5,15 @@ const Map = () => {
 
   return (
     <div>
-      I am a map
+      I am a 
+      This is where the drop-down menu will live
+      We will render this in app.
+      
     </div>
   )
 }
 
 export default Map;
+//This will contain our drop-down menu which will need to be populated with the country names
+//We will take the prop and map over to display all names to render the drop-down
+//Testing needed
