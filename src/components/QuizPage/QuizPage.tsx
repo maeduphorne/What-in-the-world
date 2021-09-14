9d4101516de77d309c877cbc9c92f44c938114a5
Mark Cawthray <dr.cawthray@gmail.com>
2021-09-13 19:03:02 -0600

    Update interface to accept new structure

--- src/components/QuizPage/QuizPage.tsx
+++ src/components/QuizPage/QuizPage.tsx
@@ -6,6 +6,7 @@ interface IState{
   currentCountry: {
     name: string
   } | undefined
+  country: string
 }
 
 const QuizPage: React.FC<IState> = ({questions, currentCountry}) => {
@@ -19,29 +20,29 @@ const QuizPage: React.FC<IState> = ({questions, currentCountry}) => {
   }
 
   useEffect(() =>{
-   if(!quizQuestions) {
-     getRandomElement()
-   }
+    if(!quizQuestions) {
+      getRandomElement()
+    }
   }, [quizQuestions])
   
   return (
     <div>
-     <section>
-       {currentCountry && <h2>{quizQuestions} {currentCountry.name}</h2>}
-       {currentCountry && <Form questions={questions} currentCountry={currentCountry}/>}
-     </section>
+      <section>
+        {currentCountry && <h2>{quizQuestions} {currentCountry.name}</h2>}
+        {currentCountry && <Form questions={questions} currentCountry={currentCountry}/>}
+      </section>
     </div>
   )
 }
 
 export default QuizPage;
 
-//This will be our overarching page that renders the initial quiz(form), the answers (which will also have the CountryInfo)
-// [X] function that randomizes the quiz question and passes the correct info to the form component to be compared with the form input values
-// [x] props of questions to Form component
-// [X] This can hold state of the selected country and state of an array of 4 questions(strings)
-// [x] This will render form that holds a randomized question 
-// write helper function that compares input values to country object, set state for 'quizResults' and pass 'quizResults' as prop to Answers Component
-// Testing needed
+// //This will be our overarching page that renders the initial quiz(form), the answers (which will also have the CountryInfo)
+// // [X] function that randomizes the quiz question and passes the correct info to the form component to be compared with the form input values
+// // [x] props of questions to Form component
+// // [X] This can hold state of the selected country and state of an array of 4 questions(strings)
+// // [x] This will render form that holds a randomized question 
+// // write helper function that compares input values to country object, set state for 'quizResults' and pass 'quizResults' as prop to Answers Component
+// // Testing needed
 
-// [ ] Need to add Link Router to take user to a QuizPage component but not render on main page
+// // [ ] Need to add Link Router to take user to a QuizPage component but not render on main page
