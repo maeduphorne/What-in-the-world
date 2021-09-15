873aa3a86cbde4d76927ab4b016699f5ebcf6ec2
Mae Duphorne <maelduphorne@gmail.com>
2021-09-15 10:23:52 -0600

    Add first describe block and test

--- cypress/integration/app_spec.js
+++ cypress/integration/app_spec.js
@@ -1,5 +1,12 @@
-describe('User App Main Page Flows', () => {
-    beforeEach(() => {
-    cy.intercept()
-    .visit('http://localhost:3000/')
-  });
+// describe('User App Main Page Flows', () => {
+//     beforeEach(() => {
+//     cy.visit('http://localhost:3000/')
+//     })
+// });
+
+describe('Main Page Render', () => {
+    it('Should have a header with the App\'s name on page load', () => {
+        cy.visit('http://localhost:3000/')
+        cy.get('h1').contains('What In The World')
+    })
+})
