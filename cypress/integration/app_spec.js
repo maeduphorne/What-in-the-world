66d579fc96c4965b565832ecce5b46de41037879
Mae Duphorne <maelduphorne@gmail.com>
2021-09-15 10:53:05 -0600

    Add test for selecting a country

--- cypress/integration/app_spec.js
+++ cypress/integration/app_spec.js
@@ -5,15 +5,18 @@
 // });
 
 describe('Main Page Render', () => {
-    it('Should have a header with the App\'s name on page load', () => {
+    beforeEach(() => {
         cy.visit('http://localhost:3000/')
+        });
+
+    it('Should have a header with the App\'s name on page load', () => {
         cy.get('h1').contains('What In The World')
     })
 
     // should display a drop down form of countries on load
     it('Should display a drop down on load & populate the selections with country names', () => {
-        cy.visit('http://localhost:3000/')
         cy.get('.country-dropdown').should('be.visible')
+        .select('Uzbekistan').should('be.visible')
     })
     // should be able to select a country and submit the country on button click 
 })
