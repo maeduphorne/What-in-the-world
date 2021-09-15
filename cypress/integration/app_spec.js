23456d4a7f80a0ad2689ae963bbccb91c771ced5
Mae Duphorne <maelduphorne@gmail.com>
2021-09-15 10:37:22 -0600

    Check for dropdown

--- cypress/integration/app_spec.js
+++ cypress/integration/app_spec.js
@@ -9,4 +9,11 @@ describe('Main Page Render', () => {
         cy.visit('http://localhost:3000/')
         cy.get('h1').contains('What In The World')
     })
+
+    // should display a drop down form of countries on load
+    it('Should display a drop down on load & populate the selections with country names', () => {
+        cy.visit('http://localhost:3000/')
+        cy.get('.country-dropdown').should('be.visible')
+    })
+    // should be able to select a country and submit the country on button click 
 })
