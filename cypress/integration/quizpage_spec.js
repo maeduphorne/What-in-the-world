31c1cd4c161ec4cfbd9642112c148e907eb4ae84
Mae Duphorne <maelduphorne@gmail.com>
2021-09-16 17:48:13 -0600

    Test page render

--- cypress/integration/quizpage_spec.js
+++ cypress/integration/quizpage_spec.js
@@ -1,5 +1,14 @@
 describe('Quiz Page User Flow', () => {
     beforeEach(() => {
-        cy.visit('http://localhost:3000/')
+        cy.visit('http://localhost:3000/Uzbekistan')
     });
+
+    it('Should render a page displaying a quiz question about the selected country & answer field', () => {
+        cy.get('h1').contains('What In The World')
+        .get('h2').contains('Uzbekistan')
+        .get('.answer-input').should('be.visible')
+        .get('.submit-button').should('be.visible')
+    })
+
+
 })
