efaee15d51b9b6a52e643efbf9b8443e2851accf
Mark Cawthray <dr.cawthray@gmail.com>
2021-09-17 11:06:48 -0600

    Create check for errors function in api calls

--- src/api/apiCalls.tsx
+++ src/api/apiCalls.tsx
@@ -1,9 +1,16 @@
 const apiCalls = {
-    fetchCountriesData() {
-        return fetch('https://restcountries.eu/rest/v2/all')
-            .then((response) => response.json())
-            .catch((error) => console.log(error.message));
-    }
+  fetchCountriesData() {
+    return fetch('https://restcountries.eu/rest/v2/allhotdogs')
+    .then((response) => checkForErrors(response))
+  }  
 }
+
+const checkForErrors = (response:any) => {
+  if(response.ok) {
+    return response.json();
+  } else {
+    throw `${response.status} ERROR. Could not access server data.`
+  }
+} 
 
 export default apiCalls;
