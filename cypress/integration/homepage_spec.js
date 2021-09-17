1d22a9df9c1d9204d443af1930d177cccafbb7c7
Fara Akhatova <faraakhatova@gmail.com>
2021-09-17 10:38:17 -0600

    Add special command for before each get a fetchData

--- cypress/integration/homepage_spec.js
+++ cypress/integration/homepage_spec.js
@@ -1,6 +1,8 @@
 describe('Main Page Render', () => {
     beforeEach(() => {
         cy.visit('http://localhost:3000/')
+        cy.FetchData('GET', 'https://restcountries.eu/rest/v2/all')
+        
         });
 
     it('Should have a header with the App\'s name on page load & an image of a map', () => {
@@ -8,124 +10,46 @@ describe('Main Page Render', () => {
         cy.get('.worldMapImg').should('be.visible')
     })
 
-    // should display a drop down form of countries on load
-    it('Should display a drop down menu on load', () => {
-        cy.get('.country-dropdown')
-            .should('be.visible')
-            .should('have.value', '')
-            .contains('option')
-        })   
-    // should be able to select a country and submit the country on button click 
-    it('Should be able to select a country', () => {
-        cy.get('.country-dropdown')
-            .select('Uzbekistan')
-            .should('be.visible')
-    })
+//     // should display a drop down form of countries on load
+//     it('Should display a drop down menu on load', () => {
+//         cy.get('.country-dropdown')
+//             .should('be.visible')
+//             .should('have.value', '')
+//             .contains('option')
+//         })   
+//     // should be able to select a country and submit the country on button click 
+//     it('Should be able to select a country', () => {
+//         cy.get('.country-dropdown')
+//             .select('Uzbekistan')
+//             .should('be.visible')
+//     })
 
-    it('Should be able to select a country, should update url to matching path and display country Quiz Page', () => {
-        cy.get('.country-dropdown')
-            .select('Uzbekistan')
-            .get('.dropdown-btn').click()
-            .url().should('eq', 'http://localhost:3000/Uzbekistan')
-            .url().should('not.eq', 'http://localhost:3000/')
-            .get('h1').contains('What In The World')
-            .get('h2').contains('Uzbekistan')
-            .get('.answer-input').should('be.visible')
-    })
+//     it('Should be able to select a country, should update url to matching path and display country Quiz Page', () => {
+//         cy.get('.country-dropdown')
+//             .select('Uzbekistan')
+//             .get('.dropdown-btn').click()
+//             .url().should('eq', 'http://localhost:3000/Uzbekistan')
+//             .url().should('not.eq', 'http://localhost:3000/')
+//             .get('h1').contains('What In The World')
+//             .get('h2').contains('Uzbekistan')
+//             .get('.answer-input').should('be.visible')
+//     })
 
-})
+// })
 
-describe('Main page fetch', () => {
-    it('Should create a drop down menu using all country names on page load', () => {
-        cy.visit('http://localhost:3000/')
-        cy.intercept('GET', 'https://restcountries.eu/rest/v2/all', {
-            statusCode: 200,
-            body: {
-                    countries: 
-                    [
-                        {
-                            "name": "Uzbekistan",
-                            "topLevelDomain": [
-                            ".uz"
-                            ],
-                            "alpha2Code": "UZ",
-                            "alpha3Code": "UZB",
-                            "callingCodes": [
-                            "998"
-                            ],
-                            "capital": "Tashkent",
-                            "altSpellings": [
-                            "UZ",
-                            "Republic of Uzbekistan",
-                            "O‘zbekiston Respublikasi",
-                            "Ўзбекистон Республикаси"
-                            ],
-                            "region": "Asia",
-                            "subregion": "Central Asia",
-                            "population": 31576400,
-                            "latlng": [
-                            41,
-                            64
-                            ],
-                            "demonym": "Uzbekistani",
-                            "area": 447400,
-                            "gini": 36.7,
-                            "timezones": [
-                            "UTC+05:00"
-                            ],
-                            "borders": [
-                            "AFG",
-                            "KAZ",
-                            "KGZ",
-                            "TJK",
-                            "TKM"
-                            ],
-                            "nativeName": "O‘zbekiston",
-                            "numericCode": "860",
-                            "currencies": [
-                            {
-                            "code": "UZS",
-                            "name": "Uzbekistani so'm",
-                            "symbol": null
-                            }
-                            ],
-                            "languages": [
-                            {
-                            "iso639_1": "uz",
-                            "iso639_2": "uzb",
-                            "name": "Uzbek",
-                            "nativeName": "Oʻzbek"
-                            },
-                            {
-                            "iso639_1": "ru",
-                            "iso639_2": "rus",
-                            "name": "Russian",
-                            "nativeName": "Русский"
-                            }
-                            ],
-                            "translations": {
-                            "de": "Usbekistan",
-                            "es": "Uzbekistán",
-                            "fr": "Ouzbékistan",
-                            "ja": "ウズベキスタン",
-                            "it": "Uzbekistan",
-                            "br": "Uzbequistão",
-                            "pt": "Usbequistão",
-                            "nl": "Oezbekistan",
-                            "hr": "Uzbekistan",
-                            "fa": "ازبکستان"
-                            },
-                            "flag": "https://restcountries.eu/data/uzb.svg",
-                            "regionalBlocs": [],
-                            "cioc": "UZB"
-                        }
-                    ]
-                }
-        })
-        cy.get('.country-dropdown').select('Uzbekistan')
-        cy.get('.dropdown-btn').click()
-        .get('h2').contains('Uzbekistan')
-        .get('.answer-input').should('be.visible')
-        .get('.submit-button').should('be.visible')
-    }) 
+// describe('Main page fetch', () => {
+//     it('Should create a drop down menu using all country names on page load', () => {
+       
+//         cy.get('.country-dropdown').select('Uzbekistan')
+//         cy.get('.dropdown-btn').click()
+//         .get('h2').contains('Uzbekistan')
+//         .get('.answer-input').should('be.visible')
+//         .get('.submit-button').should('be.visible')
+//     }) 
+//     it('Should select a country and move to that country\'s quiz page', () => {
+//         cy.get('.country-selector')
+//         .select('Uzbekistan')
+//         .get('.dropdown-btn').click()
+//         // .url().should('eq', 'http://localhost:3000/Uzbekistan')
+// })
 })
--- cypress/integration/quizpage_spec.js
+++ cypress/integration/quizpage_spec.js
@@ -1,28 +1,26 @@
-const { useEffect } = require("react");
-var questions = require('../fixtures/questions.json');
 
 describe('Quiz Page User Flow', () => {
     beforeEach(() => {
-        it('Should select a country and move to that country\'s quiz page', () => {
-            cy.get('.country-dropdown')
-                .select('Uzbekistan')
-                .get('.dropdown-btn').click()
-                .url().should('eq', 'http://localhost:3000/Uzbekistan')
-        })
-    });
-
-    it('Should render a page displaying a quiz question about the selected country & answer field', () => {
-        cy.get('h1').contains('What In The World')
-        .get('h2').contains('Uzbekistan')
-        .get('.answer-input').should('be.visible')
-        .get('.submit-button').should('be.visible')
+        cy.visit('http://localhost:3000/Uzbekistan')
+        // // // .url().should('eq', 'http://localhost:3000/Uzbekistan')
+        // // cy.visit('http://localhost:3000/Uzbekistan')
     })
 
-    // SAD PATH
-    it('Should render an error message & stay on form page if a user enters special characters', () => {
-        cy.get('.answer-input').type('@@@')
-        cy.get('.input-error-message').should('be.visible')
-    })
+    
+   
+
+    // it('Should render a page displaying a quiz question about the selected country & answer field', () => {
+    //     cy.get('h1').contains('What In The World')
+    //     .get('h2').contains('Uzbekistan')
+    //     .get('.answer-input').should('be.visible')
+    //     .get('.submit-button').should('be.visible')
+    // })
+
+    // // SAD PATH
+    // it('Should render an error message & stay on form page if a user enters special characters', () => {
+    //     cy.get('.answer-input').type('@@@')
+    //     cy.get('.input-error-message').should('be.visible')
+    // })
 
     // stub and pass data to question field - 1 for capital && 1 for borders
     // it Should allow a user to input an answer to the question and submit
@@ -81,24 +79,25 @@ describe('Quiz Page User Flow', () => {
         // cy.should('eq', questions[1])
     // })
 
-    it('Should tell the user if their guess was correct or incorrect', () => {
-        cy.get('.user-guess').contains('Your guess was')
-        //Will need to change the value below based on what text is entered as a response
-        cy.get('h3').contains('Incorrect')
-    })
+    // it('Should tell the user if their guess was correct or incorrect', () => {
+    //     cy.get('.user-guess').contains('Your guess was')
+    //     //Will need to change the value below based on what text is entered as a response
+    //     cy.get('h3').contains('Incorrect')
+    // })
 
-    it('Should render text telling the user about the country', () => {
-        cy.get('.subregion-fact').contains('Central Asia')
-        //Add more facts referencing their className in the <p> tag
-    })
+    // it('Should render text telling the user about the country', () => {
+    //     cy.get('.subregion-fact').contains('Central Asia')
+    //     //Add more facts referencing their className in the <p> tag
+    // })
 
-    it('Should bring the user back to the home page by clicking the Header', () => {
-        cy.get('.home-click').click()
-        cy.get('h1').contains('What In The World')
-        cy.get('.worldMapImg').should('be.visible')
-        cy.get('.country-dropdown')
-            .should('be.visible')
-            .should('have.value', '')
-    })
+    // it('Should bring the user back to the home page by clicking the Header', () => {
+    //     cy.get('.home-click').click()
+    //     cy.get('h1').contains('What In The World')
+    //     cy.get('.worldMapImg').should('be.visible')
+    //     cy.get('.country-dropdown')
+    //         .should('be.visible')
+    //         .should('have.value', '')
+    // })
 
+// })
 })
