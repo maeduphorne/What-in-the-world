8fd8c9ce8c181f9cbd9993bd3438ee5942e73fee
Fara Akhatova <faraakhatova@gmail.com>
2021-09-17 10:40:43 -0600

    Comment out double test for same component

--- cypress/integration/homepage_spec.js
+++ cypress/integration/homepage_spec.js
@@ -11,29 +11,30 @@ describe('Main Page Render', () => {
     })
 
 //     // should display a drop down form of countries on load
-//     it('Should display a drop down menu on load', () => {
-//         cy.get('.country-dropdown')
-//             .should('be.visible')
-//             .should('have.value', '')
-//             .contains('option')
-//         })   
+    it('Should display a drop down menu on load', () => {
+        cy.get('.country-dropdown')
+            .should('be.visible')
+            .should('have.value', '')
+            .contains('option')
+        })   
 //     // should be able to select a country and submit the country on button click 
-//     it('Should be able to select a country', () => {
-//         cy.get('.country-dropdown')
-//             .select('Uzbekistan')
-//             .should('be.visible')
-//     })
 
-//     it('Should be able to select a country, should update url to matching path and display country Quiz Page', () => {
-//         cy.get('.country-dropdown')
-//             .select('Uzbekistan')
-//             .get('.dropdown-btn').click()
-//             .url().should('eq', 'http://localhost:3000/Uzbekistan')
-//             .url().should('not.eq', 'http://localhost:3000/')
-//             .get('h1').contains('What In The World')
-//             .get('h2').contains('Uzbekistan')
-//             .get('.answer-input').should('be.visible')
-//     })
+    // it('Should be able to select a country', () => {
+    //     cy.get('.country-dropdown')
+    //         .select('Uzbekistan')
+    //         .should('be.visible')
+    // })
+
+    it('Should be able to select any country, should update url to matching path and display country Quiz Page', () => {
+        cy.get('.country-dropdown')
+            .select('Uzbekistan')
+            .get('.dropdown-btn').click()
+            .url().should('eq', 'http://localhost:3000/Uzbekistan')
+            .url().should('not.eq', 'http://localhost:3000/')
+            .get('h1').contains('What In The World')
+            .get('h2').contains('Uzbekistan')
+            .get('.answer-input').should('be.visible')
+    })
 
 // })
 
