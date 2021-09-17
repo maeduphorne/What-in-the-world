e51586dabe491b94f0420c0dda6a490ca020f6c4
Mae Duphorne <maelduphorne@gmail.com>
2021-09-16 18:27:36 -0600

    pseudocode tests to add

--- cypress/integration/quizpage_spec.js
+++ cypress/integration/quizpage_spec.js
@@ -1,6 +1,6 @@
 describe('Quiz Page User Flow', () => {
     beforeEach(() => {
-        it('Should select a sountry and move to that country\'s quiz page', () => {
+        it('Should select a country and move to that country\'s quiz page', () => {
             cy.get('.country-dropdown')
                 .select('Uzbekistan')
                 .get('.dropdown-btn').click()
@@ -14,7 +14,12 @@ describe('Quiz Page User Flow', () => {
         .get('.answer-input').should('be.visible')
         .get('.submit-button').should('be.visible')
     })
-
+    // stub and pass data to question field - 1 for capital && 1 for borders
+    // it Should allow a user to input an answer to the question and submit
+    // it Should render text telling the user if the answer is incorrect or correct
+    // it Should render text telling the user some facts about the country
+    // SAD - if the user enter secial characters, an error messages is visible
+    // it Should bring the user back to the home page by clicking the Header
 
 
 })
