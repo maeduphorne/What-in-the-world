6bcfc5416e357a6aa9cf5fae112c2843aa7cb677
Mark Cawthray <dr.cawthray@gmail.com>
2021-09-18 13:44:43 -0600

    Write tests for error page user flows

--- cypress/integration/errorpage_spec.js
+++ cypress/integration/errorpage_spec.js
@@ -1,4 +1,36 @@
-describe('Error page user flow', () => {
+const { createYield } = require("typescript")
 
-  
+describe('404 Error page user flow', () => {
+  beforeEach(() => {
+    cy.Fetch404Data('GET', 'https://restcountries.eu/rest/v2/all')
+    cy.visit('http://localhost:3000')
+  });
+
+  it('Should have a header with the App\'s name on page load & an error image', () => {
+    cy.get('h1').contains('What In The World')
+    cy.get('.error-img').should('be.visible')
+  })
+
+  it('User should see a message that displays the type of error', () => {
+    cy.get('.error-message').contains('404 ERROR. Could not access server data.')
+  })
+
+  it('Should bring the user back to the home page by clicking the Header', () => {
+    cy.get('.home-click').click()
+    cy.get('h1').contains('What In The World')
+    cy.get('.worldMapImg').should('be.visible')
+    cy.get('.country-dropdown')
+        .should('be.visible')
+        .should('have.value', '')
+})
+
+it('Should bring the user back to the home page by clicking the take me home button', () => {
+  cy.get('button').click()
+  cy.get('h1').contains('What In The World')
+  cy.get('.worldMapImg').should('be.visible')
+  cy.get('.country-dropdown')
+      .should('be.visible')
+      .should('have.value', '')
+})
+
 })
